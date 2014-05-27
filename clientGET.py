7d55b813cb6bb004f71199ef53f6861e0c68aad5
chrysn <chrysn@fsfe.org>
2014-05-27 14:43:17 +0200

    use observation cancellation in example

--- clientGET.py
+++ clientGET.py
@@ -44,7 +44,14 @@ class Agent():
         request.opt.uri_path = ('time',)
         request.remote = ("127.0.0.1", coap.COAP_PORT)
         request.opt.observe = 0
-        d = protocol.request(request, observeCallback=self.printLaterResponse)
+
+        # this would be usually done as self.protocol.request(request, o...),
+        # but we need to access the observation to cancel it
+        requester = coap.Requester(self.protocol, request, observeCallback=self.printLaterResponse, block1Callback=None, block2Callback=None, observeCallbackArgs=None, block1CallbackArgs=None, block2CallbackArgs=None, observeCallbackKeywords=None, block1CallbackKeywords=None, block2CallbackKeywords=None)
+        self.observation = requester.observation
+        reactor.callLater(15, self.stopObserving)
+        d = requester.deferred
+
         d.addCallback(self.printResponse)
         d.addErrback(self.noResponse)
 
@@ -59,6 +66,12 @@ class Agent():
         print 'Failed to fetch resource:'
         print failure
         #reactor.stop()
+
+    def stopObserving(self):
+        print 'Not interested in the resource any more, staying alive for some time to see the observation results bounce back'
+        self.observation.cancel()
+
+        reactor.callLater(10, reactor.stop)
 
 logging.basicConfig(level=logging.INFO)
 
