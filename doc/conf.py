cc1dc6b42ee2bdd4922a4f3be61c0faa3cd82c89
chrysn <chrysn@fsfe.org>
2014-06-04 12:50:25 +0200

    experimenting with options for readthedocs

--- doc/conf.py
+++ doc/conf.py
@@ -19,6 +19,8 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
+# maybe required for readthedocs
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
