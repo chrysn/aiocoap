f2a92f364e0bf511f3a5d1340e4e0fa3927a6f7e
chrysn <chrysn@fsfe.org>
2014-06-04 13:15:46 +0200

    common license headers in more files

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+# This file is part of the txThings project.
+#
+# Copyright (c) 2012-2014 Maciej Wasilak <http://sixpinetrees.blogspot.com/>,
+#               2013-2014 Christian Amsüss <c.amsuess@energyharvesting.at>
+#
+# txThings is free software, this file is published under the MIT license as
+# described in the accompanying LICENSE file.
+
 from setuptools import setup, find_packages
 
 name = "txThings asyncio branch"
