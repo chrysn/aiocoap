6710d98d105a8cbf0fc8c40597f7633508024973
chrysn <chrysn@fsfe.org>
2014-06-11 08:58:47 +0200

    setup.py overhaul

--- setup.py
+++ setup.py
@@ -8,19 +8,36 @@
 # txThings is free software, this file is published under the MIT license as
 # described in the accompanying LICENSE file.
 
+"""aiocoap is a Python library for writing servers and clients for the CoAP
+(Constrained Application) Protocol, which is used mainly in the context of IoT
+(Internet of Things) devices."""
+
 from setuptools import setup, find_packages
 
 name = "aiocoap"
 version = "0.1-development"
+description = "Python CoAP library"
+longdescription = __doc__
 
 setup(
     name=name,
     version=version,
+    description=description,
     packages=find_packages(),
-    license="MIT",
 
     author="Maciej Wasilak, Christian Amsüss",
-    author_email=", c.amsuess@energyharvesting.at",
+    author_email="Christian Amsüss <c.amsuess@energyharvesting.at>",
+
+    keywords=['coap', 'asyncio', 'iot'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
 
     command_options={
         'build_sphinx': {
