d17279e1ae19f955da6e6c844e2313dca2175c0a
chrysn <chrysn@fsfe.org>
2014-09-17 05:25:48 +0200

    resource documentation

--- aiocoap/resource.py
+++ aiocoap/resource.py
@@ -1,11 +1,30 @@
 # This file is part of the Python aiocoap library project.
 #
-# Copyright (c) Twisted Matrix Laboratories,
-#               2012-2014 Maciej Wasilak <http://sixpinetrees.blogspot.com/>,
+# Copyright (c) 2012-2014 Maciej Wasilak <http://sixpinetrees.blogspot.com/>,
 #               2013-2014 Christian Amsüss <c.amsuess@energyharvesting.at>
 #
 # aiocoap is free software, this file is published under the MIT license as
 # described in the accompanying LICENSE file.
+
+"""Basic resource implementations
+
+A resource in URL / CoAP / REST terminology is the thing identified by a URI.
+
+Here, a :class:`.Resource` is the place where server functionality is
+implemented. In many cases, there exists one persistent Resource object for a
+given resource (eg. a ``TimeResource()`` is responsible for serving the
+``/time`` location). On the other hand, an aiocoap server context accepts only
+one thing as its serversite, and that is a Resource too (typically of the
+:class:`Site` class).
+
+Resources are most easily implemented by deriving from :class:`.Resource` and
+implementing ``render_GET``, ``render_POST`` and similar coroutine methods.
+Those take a single request message object and must return a
+:class:`aiocoap.Message` object.
+
+To serve more than one resource on a site, use the :class:`Site` class to
+dispatch requests based on the Uri-Path header.
+"""
 
 import asyncio
 
@@ -13,47 +32,21 @@ from . import error
 from . import interfaces
 
 class Resource(interfaces.Resource):
-    """
-    CoAP-accessible resource.
+    """Simple base implementation of the :class:`interfaces.Resource`
+    interface
 
+    The render method delegates content creation to ``render_$METHOD`` methods,
+    and responds appropriately to unsupported methods.
     """
 
     @asyncio.coroutine
     def render(self, request):
-        """
-        Render a given resource. See L{IResource}'s render method.
-
-        I delegate to methods of self with the form 'render_METHOD'
-        where METHOD is the HTTP that was used to make the
-        request. Examples: render_GET, render_HEAD, render_POST, and
-        so on. Generally you should implement those methods instead of
-        overriding this one.
-
-        render_METHOD methods are expected to return a string which
-        will be the rendered page, unless the return value is
-        twisted.web.server.NOT_DONE_YET, in which case it is this
-        class's responsibility to write the results to
-        request.write(data), then call request.finish().
-
-        Old code that overrides render() directly is likewise expected
-        to return a string or NOT_DONE_YET.
-        """
         if not request.code.is_request():
             raise error.UnsupportedMethod()
         m = getattr(self, 'render_%s' % request.code, None)
         if not m:
             raise error.UnallowedMethod()
         return m(request)
-
-    def generate_resource_list(self, data, path=""):
-        params = self.encode_params() + (";obs" if self.observable else "")
-        if self.visible is True:
-            if path is "":
-                data.append('</>' + params)
-            else:
-                data.append('<' + path + '>' + params)
-        for key in self.children:
-            self.children[key].generate_resource_list(data, path + "/" + key)
 
 class ObservableResource(Resource, interfaces.ObservableResource):
     def __init__(self):
@@ -78,7 +71,10 @@ class Site(interfaces.ObservableResource):
     all the resources that can be found when the endpoint gets accessed as a
     server.
 
-    This provides easy registration of statical resources."""
+    This provides easy registration of statical resources.
+
+    Add resources at absolute locations using the :meth:`.add_observation`
+    method."""
 
     def __init__(self):
         self._resources = {}
@@ -105,4 +101,4 @@ class Site(interfaces.ObservableResource):
             pass
 
     def add_resource(self, path, resource):
-        self._resources[path] = resource
+        self._resources[tuple(path)] = resource
