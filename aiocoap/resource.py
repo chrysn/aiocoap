e1409b6af913a38da5c6280db8475a669a7decc7
chrysn <chrysn@fsfe.org>
2014-10-05 14:58:25 +0200

    resource: introduce hashing_etag
    this should provide a usable poor man's way of providing an etag.

--- aiocoap/resource.py
+++ aiocoap/resource.py
@@ -26,10 +26,45 @@ To serve more than one resource on a site, use the :class:`Site` class to
 dispatch requests based on the Uri-Path header.
 """
 
+import hashlib
 import asyncio
 
 from . import error
 from . import interfaces
+from . import numbers
+
+def hashing_etag(request, response):
+    """Helper function for do_GET handlers that allows them to use ETags based
+    on the payload's hash value
+
+    Run this on your request and response before returning from do_GET; it is
+    safe to use this function with all kinds of responses, it will only act on
+    2.05 Content. The hash used are the first 8 bytes of the sha1 sum of the
+    payload.
+
+    Note that this method is not ideal from a server performance point of view
+    (a file server, for example, might want to hash only the stat() result of a
+    file instead of reading it in full), but it saves bandwith for the simple
+    cases.
+
+    >>> from aiocoap import *
+    >>> req = Message(code=GET)
+    >>> hash_of_hello = b'\\xaa\\xf4\\xc6\\x1d\\xdc\\xc5\\xe8\\xa2'
+    >>> req.opt.etags = [hash_of_hello]
+    >>> resp = Message(code=CONTENT)
+    >>> resp.payload = b'hello'
+    >>> hashing_etag(req, resp)
+    >>> resp                                            # doctest: +ELLIPSIS
+    <aiocoap.Message at ... 2.03 Valid ... 1 option(s)>
+    """
+
+    if response.code != numbers.codes.CONTENT:
+        return
+
+    response.opt.etag = hashlib.sha1(response.payload).digest()[:8]
+    if request.opt.etags is not None and response.opt.etag in request.opt.etags:
+        response.code = numbers.codes.VALID
+        response.payload = b''
 
 class Resource(interfaces.Resource):
     """Simple base implementation of the :class:`interfaces.Resource`
