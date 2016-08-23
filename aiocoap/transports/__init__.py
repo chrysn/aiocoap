c988e8b1df70e9d79f9850dc0a63d9c44c52ba64
Geaaru <geaaru@gmail.com>
2016-08-23 18:02:40 +0200

    Add __init__.py file under transports
    The directory without an __init__ file caused issues with
    setuptools-based deployment.
    
    Closes: https://github.com/chrysn/aiocoap/pull/42

---
+++ aiocoap/transports/__init__.py
@@ -0,0 +1,10 @@
+# This file is part of the Python aiocoap library project.
+#
+# Copyright (c) 2012-2014 Maciej Wasilak <http://sixpinetrees.blogspot.com/>,
+#               2013-2014 Christian Amsüss <c.amsuess@energyharvesting.at>
+#
+# aiocoap is free software, this file is published under the MIT license as
+# described in the accompanying LICENSE file.
+
+"""Container module for transports
+"""
