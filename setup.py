ff254232c224752c348601fb4fb144852590158d
chrysn <chrysn@fsfe.org>
2016-12-09 13:46:37 +0100

    Bump version to 0.3-rc1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 from distutils.core import Command
 
 name = "aiocoap"
-version = "0.2"
+version = "0.3-rc1"
 description = "Python CoAP library"
 longdescription = __doc__
 
