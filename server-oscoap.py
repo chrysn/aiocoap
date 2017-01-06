690171e7ad09911736c9890a9b7ae9ddfd20fb4f
chrysn <chrysn@fsfe.org>
2017-01-06 22:36:07 +0100

    OSCOAP: add example server

---
+++ server-oscoap.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+# This file is part of the Python aiocoap library project.
+#
+# Copyright (c) 2012-2014 Maciej Wasilak <http://sixpinetrees.blogspot.com/>,
+#               2013-2014 Christian Amsüss <c.amsuess@energyharvesting.at>
+#
+# aiocoap is free software, this file is published under the MIT license as
+# described in the accompanying LICENSE file.
+
+import asyncio
+
+import aiocoap.resource as resource
+import aiocoap.error as error
+import aiocoap
+import aiocoap.oscoap as oscoap
+from aiocoap.util import hostportjoin
+
+from server import BlockResource, SeparateLargeResource, TimeResource
+
+class PleaseUseOscoap(error.ConstructionRenderableError):
+    code = aiocoap.UNAUTHORIZED
+    message = "Please use OSCOAP"
+
+class PrivateSite(resource.Site):
+    def __init__(self, *, contexts=[]):
+        super().__init__()
+        self.contexts = {c.cid: c for c in contexts}
+
+    @asyncio.coroutine
+    def render(self, request):
+        try:
+            cid, sid = oscoap.verify_start(request)
+        except oscoap.NotAProtectedMessage:
+            raise PleaseUseOscoap()
+
+        # right now we'll rely on the sid to match, especially as it's not sent
+        # unconditionally anyway
+        try:
+            sc = self.contexts[cid]
+        except KeyError:
+            raise PleaseUseOscoap() # may we disclose the reason?
+
+        unprotected, seqno = sc.unprotect(request)
+
+        # FIXME the render doesn't provide a way to provide context in the
+        # sense of "who is the user"; obviously, the render interface needs
+        # rework
+        response = (yield from super().render(unprotected))
+
+        if response.code is None:
+            # FIXME: this duplicates the default setting in aiocoap.protocol
+            response.code = aiocoap.CONTENT
+        # FIXME: this will be present on the client because their Request
+        # object populates it; not sure yet how/where this should be passed
+        # here; worse, only setting requested_hostinfo possibly doesn't
+        # cover proxy uris or schemes
+        response.requested_hostinfo = hostportjoin(request.opt.uri_host, request.opt.uri_port)
+        response.requested_scheme = 'coap'
+        response.requested_query = ()
+        response.requested_path = ()
+
+        protected_response, _ = sc.protect(response, request_seq=seqno)
+
+        return protected_response
+
+def main():
+    # Resource tree creation
+    root = PrivateSite(contexts=[oscoap.FilesystemSecurityContext('./demo-context-recipient', 'recipient')])
+
+    root.add_resource(('.well-known', 'core'), resource.WKCResource(root.get_resources_as_linkheader))
+    root.add_resource(('time',), TimeResource())
+    root.add_resource(('other', 'block'), BlockResource())
+    root.add_resource(('other', 'separate'), SeparateLargeResource())
+
+    asyncio.Task(aiocoap.Context.create_server_context(root))
+
+    asyncio.get_event_loop().run_forever()
+
+if __name__ == "__main__":
+    main()
