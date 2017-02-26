2a172b35473796c25ff3dea97fd3ccbf7f9731fb
chrysn <chrysn@fsfe.org>
2017-02-26 22:15:15 +0100

    Options: Expose Max-Age

--- aiocoap/options.py
+++ aiocoap/options.py
@@ -170,3 +170,4 @@ class Options(object):
     proxy_scheme = _single_value_view(OptionNumber.PROXY_SCHEME)
     size1 = _single_value_view(OptionNumber.SIZE1)
     object_security = _single_value_view(OptionNumber.OBJECT_SECURITY)
+    max_age = _single_value_view(OptionNumber.MAX_AGE)
