5181582535fb487188885c571b891a2cccedc2ad
chrysn <chrysn@fsfe.org>
2017-02-26 11:14:15 +0100

    OSCOAP plug test: tests 11-15

--- contrib/oscoap-plugtest/plugtest_common.py
+++ contrib/oscoap-plugtest/plugtest_common.py
@@ -13,18 +13,24 @@ def get_security_context(testno, role):
         json.dump(secretdata, out)
     settingsdata = json.load(open('./common-context/settings.json'))
     with open(os.path.join(contextcopy, 'settings.json'), 'w') as out:
+        # this needs to be messed with early, as key properties are derived
+        # from this data
         if testno == 10 and role == 'sender':
-            # this needs to be messed with early, as key properties are derived
-            # from this data
-            print(settingsdata)
             assert settingsdata['sender-id'] == '636C69656E74'
             settingsdata['sender-id'] = '116C69656E74'
+        if testno == 13 and role == 'sender':
+            settingsdata['recipient-id'] = '116572766572'
+
         json.dump(settingsdata, out)
     sequence = {
             "used": {(settingsdata['recipient-id'] if role == 'recipient' else settingsdata['sender-id']).lower(): testno},
             "seen": {(settingsdata['sender-id'] if role == 'recipient' else settingsdata['recipient-id']).lower(): list(range(testno))}
         }
     with open(os.path.join(contextcopy, 'sequence.json'), 'w') as out:
+        if testno == 14 and role == 'sender':
+            sequence["used"][list(sequence["used"].keys())[0]] = 0;
+        if testno == 15 and role == 'sender':
+            sequence["seen"][list(sequence["seen"].keys())[0]] = [65];
         json.dump(sequence, out)
     print("Temporary context with seqno %d copied to %s"%(testno, contextcopy))
     secctx = oscoap.FilesystemSecurityContext(contextcopy, role=role)
@@ -36,6 +42,11 @@ def get_security_context(testno, role):
     if testno == 9 and role == 'sender':
         assert hexlify(secctx.my_iv) == b'e828a479d088c4'
         secctx.my_iv = unhexlify(b'1128a479d088c4')
+    if testno == 11 and role == 'sender':
+        secctx.other_key = bytes((11,)) + secctx.other_key[1:]
+    if testno == 12 and role == 'sender':
+        assert hexlify(secctx.other_iv) == b'58f91a5cdff4f5'
+        secctx.other_iv = unhexlify(b'11f91a5cdff4f5')
 
     return secctx
 
