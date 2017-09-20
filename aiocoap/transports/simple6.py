d322889635848c771d91c4b5f4899e0f5d8224ec
michieldwitte <michieldwitte@gmail.com>
2017-09-20 12:23:26 +0200

    Windows fix
    This makes it possible to perform GET requests to coap://coap.me in windows

--- aiocoap/transports/simple6.py
+++ aiocoap/transports/simple6.py
@@ -161,7 +161,7 @@ class _DatagramClientSocketpoolSimple6:
         ready = asyncio.Future()
         transport, protocol = yield from self._loop.create_datagram_endpoint(
                 lambda: _Connection(lambda: ready.set_result(None), self._new_message_callback, self._new_error_callback, sockaddr),
-                family=socket.AF_INET6,
+                #family=socket.AF_INET6,
                 flags=socket.AI_V4MAPPED,
                 remote_addr=sockaddr)
         yield from ready
