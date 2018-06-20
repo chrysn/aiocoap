7dd125a5dac96f7a50d1d35e51d509daba699380
chrysn <chrysn@fsfe.org>
2018-06-20 19:24:21 +0200

    put example: use uri parameter rahter than setting path components manually
    Manipulating the path stays possible but is not something that should be
    shown to newcomers first time they use the library, as it is much more
    prone to errors.

--- clientPUT.py
+++ clientPUT.py
@@ -30,11 +30,7 @@ async def main():
     await asyncio.sleep(2)
 
     payload = b"The quick brown fox jumps over the lazy dog.\n" * 30
-    request = Message(code=PUT, payload=payload)
-    # These direct assignments are an alternative to setting the URI like in
-    # the GET example:
-    request.opt.uri_host = '127.0.0.1'
-    request.opt.uri_path = ("other", "block")
+    request = Message(code=PUT, payload=payload, uri="coap://localhost/other/block")
 
     response = await context.request(request).response
 
