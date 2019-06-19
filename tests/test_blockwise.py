827c1fc87e58804cd0d1617d14df897a68bd4f5b
chrysn <chrysn@fsfe.org>
2019-06-19 22:13:16 +0200

    blockwise: Add tests for application control of block size
    This adds a failing test for the bug Ken discovered in
    https://github.com/chrysn/aiocoap/pull/158

--- tests/test_blockwise.py
+++ tests/test_blockwise.py
@@ -15,9 +15,23 @@ import unittest
 import aiocoap
 import aiocoap.defaults
 
-from .test_server import WithTestServer, WithClient, no_warnings, asynctest
+from .test_server import WithTestServer, WithClient, no_warnings, asynctest, BigResource, TestingSite
 
-class TestBlockwise(WithTestServer, WithClient):
+class BigChunkyResource(BigResource):
+    async def render_get(self, request):
+        request.remote.maximum_block_size_exp = 3
+        return await super().render_get(request)
+
+class ChunkyTestingSite(TestingSite):
+    def __init__(self):
+        super().__init__()
+
+        self.add_resource(["big", "chunky"], BigChunkyResource())
+
+class WithChunkyTestServer(WithTestServer):
+    TestingSite = ChunkyTestingSite
+
+class TestBlockwise(WithChunkyTestServer, WithClient):
     # tracked as https://github.com/chrysn/aiocoap/issues/58; behavior can be successful more or less by chance
     @unittest.skip
     @no_warnings
@@ -46,3 +60,49 @@ class TestBlockwise(WithTestServer, WithClient):
             responses.append(response.payload)
 
         self.assertSetEqual(set(responses), set(x.replace(b'0', b'O') for x in (pattern1, pattern2)))
+
+    @no_warnings
+    @asynctest
+    async def test_client_hints(self):
+        """Test whether a handle_blockwise=True request takes a block2 option
+        set in it as a hint to start requesting with a low size right away
+
+        That way of hinting at size requests is not documented, but understood
+        and occasionally used; the test primarily serves to identify problems
+        the server has with initially low block sizes."""
+
+        resp = await self.client.request(
+                aiocoap.Message(
+                    uri='coap://' + self.servernetloc + '/big',
+                    block2=(0, 0, 3),
+                    code=aiocoap.GET,
+                )).response
+
+        self.assertEqual(resp.code, aiocoap.CONTENT, "Request was unsuccessful")
+        self.assertEqual(self._count_received_messages(), (len(resp.payload) + 127) // 128, "Response not chunked into 128 bytes")
+
+    @no_warnings
+    @asynctest
+    async def test_server_hints(self):
+        """Test whether the needs_blockwise server mechanism considers size
+        exponent limits of the remote.
+
+        Mutating the remote as it is done in the BigChunkyResource is not
+        documented, but helps verify that the information in
+        maximum_block_size_exp is generally used."""
+
+        resp = await self.client.request(
+                aiocoap.Message(
+                    uri='coap://' + self.servernetloc + '/big/chunky',
+                    code=aiocoap.GET,
+                )).response
+
+        self.assertEqual(resp.code, aiocoap.CONTENT, "Request was unsuccessful")
+        self.assertEqual(self._count_received_messages(), (len(resp.payload) + 127) // 128, "Response not chunked into 128 bytes")
+
+    _received_logmsg = "Incoming message <aiocoap.Message at"
+    def _count_received_messages(self):
+        # only client-side received empty-acks are counted
+        return sum(self._received_logmsg in x.msg
+                for x in self.handler
+                if x.name != 'coap-server')
