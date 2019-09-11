chrysn/aiocoap

commit 3c29d80fd699414536f2e3b1502338415b112ee4

simple6: Fix shutdown procedure
The exception (resulting from accessing the dict's key rather than its
value) was swallowed for yet unknown reasons that will be investigated
further.

aiocoap/transports/simple6.py
@@ -206,7 +206,7 @@ class _DatagramClientSocketpoolSimple6:
 
     async def shutdown(self):
         if self._sockets:
-            await asyncio.wait([s.shutdown() for s in self._sockets])
+            await asyncio.wait([s.shutdown() for s in self._sockets.values()])
         del self._sockets
 
 class MessageInterfaceSimple6(GenericMessageInterface):

commit dfa54a5955cac79f1555c9f68a1c54095686c828

asyncio.wait: Use up results
Without actually fetching the items (that are guaranteed to be complete
but still inside futures), any errors in there would be silently
discarded.

This caused the exceptions in 3c29d80f to go unnoticed.

aiocoap/protocol.py
@@ -282,7 +282,9 @@ class Context(interfaces.RequestProvider):
         for r in self._running_renderings:
             r.cancel()
 
-        await asyncio.wait([ri.shutdown() for ri in self.request_interfaces], timeout=3, loop=self.loop)
+        done, pending = await asyncio.wait([ri.shutdown() for ri in self.request_interfaces], timeout=3, loop=self.loop)
+        for item in done:
+            await item
 
     # FIXME: determine how official this should be, or which part of it is
     # public -- now that BlockwiseRequest uses it. (And formalize what can

aiocoap/transports/simple6.py
@@ -206,7 +206,9 @@ class _DatagramClientSocketpoolSimple6:
 
     async def shutdown(self):
         if self._sockets:
-            await asyncio.wait([s.shutdown() for s in self._sockets.values()])
+            done, pending = await asyncio.wait([s.shutdown() for s in self._sockets.values()])
+            for item in done:
+                await item
         del self._sockets
 
 class MessageInterfaceSimple6(GenericMessageInterface):