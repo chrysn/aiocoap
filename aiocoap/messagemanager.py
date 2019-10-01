0bea625d535d66ad2af74bf720e8728052c78bb5
chrysn <chrysn@fsfe.org>
2019-10-01 14:56:03 +0200

    message-based protocols: Ignore requests arriving to multicast addresses
    Sending a RST back to them would be a protocol violation.

--- aiocoap/messagemanager.py
+++ aiocoap/messagemanager.py
@@ -98,10 +98,13 @@ class MessageManager(interfaces.TokenInterface, interfaces.MessageManager):
                 if message.mtype is CON:
                     self._send_empty_ack(message.remote, message.mid, reason="acknowledging incoming response")
             else:
-                self.log.info("Response not recognized - sending RST.")
-                rst = Message(mtype=RST, mid=message.mid, code=EMPTY, payload='')
-                rst.remote = message.remote
-                self._send_initially(rst)
+                if message.remote.is_multicast_locally:
+                    self.log.info("Ignoring response incoming with multicast destination.")
+                else:
+                    self.log.info("Response not recognized - sending RST.")
+                    rst = Message(mtype=RST, mid=message.mid, code=EMPTY, payload='')
+                    rst.remote = message.remote
+                    self._send_initially(rst)
         else:
             self.log.warning("Received a message with code %s and type %s (those don't fit) from %s, ignoring it."%(message.code, message.mtype, message.remote))
 
