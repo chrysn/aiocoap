39d01615484a1e172c4218a83a9c20ff5f53dd32
chrysn <chrysn@fsfe.org>
2020-03-11 10:11:14 +0100

    udp6: Portability fix to non-Linux platforms
    The options required for udp6 were assumed to be portable; turns out
    some of them are Linux proprietary.
    
    Consequently, the error handling part (for which there appears to be no
    standardized API) are gated behind a platform specific flag, and the
    socknumbers module updated accordingly.
    
    Using information gained from a build of a [35569]-fixed Python, numbers
    potentially unavailable from the socket module can now fall back to
    platform dependent shipped constants. The Python-private `IN` module is
    still used, but the fallback definitions are platform-gated.
    
    It is unlikely that this should cause regressions, as the previously
    hard-coded constants were Linux specific and are still used on that
    platform.
    
    This should allow running the udp6 transport on macOS even without
    [35569] fixed, but does not change the default transport selection yet.
    
    [35569]: https://bugs.python.org/issue35569
    
    News-Section: Portability

--- aiocoap/util/socknumbers.py
+++ aiocoap/util/socknumbers.py
@@ -9,24 +9,54 @@
 """This module contains numeric constants that would be expected in the socket
 module, but are not exposed there.
 
-For some platforms (eg. python up to 3.5 on Linux), there is an IN module that
-exposes them; and they are gathered from there.
+This gathers both socket numbers that can be present in the socket module (eg.
+the PKTINFO constants) but are not in some versions (eg. on macOS before
+<https://bugs.python.org/issue35569> is fixed) and platform dependent constants
+that are not generally available at all (the ERR constants).
 
-As a fallback, the numbers are hardcoded. Any hints on where to get them from
-are appreciated; possible options are parsing C header files (at build time?)
-or interacting with shared libraries for obtaining the symbols. The right way
-would probably be including them in Python.
+Where available, the CPython-private IN module is used to obtain some platform
+specific constants.
+
+Any hints on where to get them from in a more reliable way are appreciated;
+possible options are parsing C header files (at build time?) or interacting
+with shared libraries for obtaining the symbols. The right way would probably
+be including them in Python in a "other constants defined on this platform for
+sockets" module or dictionary.
 """
 
+import sys
+
 try:
-    from IN import IPV6_RECVERR, IP_RECVERR, IPV6_PKTINFO
+    from socket import IPV6_PKTINFO, IPV6_RECVPKTINFO
+except NameError:
+    if sys.platform == 'linux':
+        # Not sure if here are any Linux builds at all where this is
+        # unavailable
+        IPV6_PKTINFO = 50
+        IPv6_RECVPKTINFO = 49
+    elif sys.platform == 'darwin':
+        # when __APPLE_USE_RFC_3542 is defined / as would be when
+        # https://bugs.python.org/issue35569 is fixed
+        IPV6_PKTINFO = 46
+        IPV6_RECVPKTINFO = 61
+    # Not attempting to make any guesses for other platforms; the udp6 module
+    # will fail to import where it needs the specifics
+
+try:
+    from IN import IPV6_RECVERR, IP_RECVERR
 except (ImportError, NameError):
-    IPV6_RECVERR = 25
-    IP_RECVERR = 11
-    IPV6_PKTINFO = 50
+    if sys.platform == 'linux':
+        IPV6_RECVERR = 25
+        IP_RECVERR = 11
 
 # for https://bitbucket.org/pypy/pypy/issues/2648/
 try:
     from socket import MSG_ERRQUEUE
 except (ImportError, NameError):
-    MSG_ERRQUEUE = 8192
+    if sys.platform == 'linux':
+        MSG_ERRQUEUE = 8192
+
+HAS_RECVERR = 'IP_RECVERR' in locals() and 'MSG_ERRQUEUE' in locals()
+"""Indicates whether the discovered constants indicate that the Linux
+`setsockopt(IPV6, RECVERR)` / `recvmsg(..., MSG_ERRQUEUE)` mechanism is
+available"""
