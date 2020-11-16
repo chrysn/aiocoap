14ba4aeff39ac407803d052f19e460faebcb7926
chrysn <chrysn@fsfe.org>
2020-11-16 10:51:57 +0100

    Demo server: Add /whoami resource
    This should help in getting authentication into the documentation later
    on.

--- server.py
+++ server.py
@@ -94,6 +94,22 @@ class TimeResource(resource.ObservableResource):
                 strftime("%Y-%m-%d %H:%M").encode('ascii')
         return aiocoap.Message(payload=payload)
 
+class WhoAmI(resource.Resource):
+    async def render_get(self, request):
+        text = ["Used protocol: %s." % request.remote.scheme]
+
+        text.append("Request came from %s." % request.remote.hostinfo)
+        text.append("The server address used %s." % request.remote.hostinfo_local)
+
+        claims = list(request.remote.authenticated_claims)
+        if claims:
+            text.append("Authenticated claims of the client: %s." % ", ".join(repr(c) for c in claims))
+        else:
+            text.append("No claims authenticated.")
+
+        return aiocoap.Message(content_format=0,
+                payload="\n".join(text).encode('utf8'))
+
 # logging setup
 
 logging.basicConfig(level=logging.INFO)
@@ -108,6 +124,7 @@ def main():
     root.add_resource(['time'], TimeResource())
     root.add_resource(['other', 'block'], BlockResource())
     root.add_resource(['other', 'separate'], SeparateLargeResource())
+    root.add_resource(['whoami'], WhoAmI())
 
     asyncio.Task(aiocoap.Context.create_server_context(root))
 
