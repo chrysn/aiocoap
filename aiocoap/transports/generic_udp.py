8f994614def54a7be68b517ed92dbe56aba072ea
chrysn <chrysn@fsfe.org>
2021-09-27 14:27:29 +0200

    generic_udp: Documentation, function sequence restructuring

--- aiocoap/transports/generic_udp.py
+++ aiocoap/transports/generic_udp.py
@@ -12,12 +12,48 @@ from aiocoap import COAP_PORT, Message
 class GenericMessageInterface(interfaces.MessageInterface):
     """GenericMessageInterface is not a standalone implementation of a
     message inteface. It does implement everything between the MessageInterface
-    and a not yet fully specified interface of "bound UDP sockets"."""
+    and a not yet fully specified interface of "bound UDP sockets".
 
-    def __init__(self, ctx: interfaces.MessageManager, log, loop):
-        self._ctx = ctx
+    It delegates sending through the address objects (which persist through
+    some time, given this is some kind of bound-socket scenario).
+
+    The user must:
+    * set up a ._pool after construction with a shutdown and a connect method
+    * provide their addresses with a send(bytes) method
+    * pass incoming data to the _received_datagram and _received_exception methods
+    """
+
+    def __init__(self, mman: interfaces.MessageManager, log, loop):
+        self._mman = mman
         self._log = log
         self._loop = loop
+
+    # Callbacks to be hooked up by the user of the class; feed data on to the
+    # message manager
+
+    def _received_datagram(self, address, datagram):
+        try:
+            message = Message.decode(datagram, remote=address)
+        except error.UnparsableMessage:
+            self._log.warning("Ignoring unparsable message from %s", address)
+            return
+
+        self._mman.dispatch_message(message)
+
+    def _received_exception(self, address, exception):
+        self._mman.dispatch_error(exception, address)
+
+    # Implementations of MessageInterface
+
+    def send(self, message):
+        if self._mman is None:
+            self._log.info("Not sending message %r: transport is already shutting down.", message)
+        else:
+            message.remote.send(message.encode())
+
+    async def shutdown(self):
+        await self._pool.shutdown()
+        self._mman = None
 
     async def determine_remote(self, request):
         if request.requested_scheme not in ('coap', None):
@@ -33,25 +69,3 @@ class GenericMessageInterface(interfaces.MessageInterface):
             raise ValueError("No location found to send message to (neither in .opt.uri_host nor in .remote)")
 
         return await self._pool.connect((host, port))
-
-    def _received_datagram(self, address, datagram):
-        try:
-            message = Message.decode(datagram, remote=address)
-        except error.UnparsableMessage:
-            self._log.warning("Ignoring unparsable message from %s", address)
-            return
-
-        self._ctx.dispatch_message(message)
-
-    def _received_exception(self, address, exception):
-        self._ctx.dispatch_error(exception, address)
-
-    def send(self, message):
-        if self._ctx is None:
-            self._log.info("Not sending message %r: transport is already shutting down.", message)
-        else:
-            message.remote.send(message.encode())
-
-    async def shutdown(self):
-        await self._pool.shutdown()
-        self._ctx = None
