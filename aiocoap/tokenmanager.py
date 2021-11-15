337cf4a3aae04ddc9b458b6e1b4c64a3b7f48683
Marko Gucanin <marko@gucanin.ch>
2021-11-15 14:27:12 +0100

    fix(tokenmanager): only use new token if not set

--- aiocoap/tokenmanager.py
+++ aiocoap/tokenmanager.py
@@ -204,7 +204,8 @@ class TokenManager(interfaces.RequestInterface, interfaces.TokenManager):
 
         # FIXME: pick a suitably short one where available, and a longer one
         # for observations if many short ones are already in-flight
-        msg.token = self.next_token()
+        if msg.token == b'':
+            msg.token = self.next_token()
 
         self.log.debug("Sending request - Token: %s, Remote: %s", msg.token.hex(), msg.remote)
 
