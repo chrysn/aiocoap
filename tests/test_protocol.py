93c8fac8ffe3f824525280e7231bddd7569d9348
chrysn <chrysn@fsfe.org>
2022-08-17 09:48:43 +0200

    tests: Adjust to removed context manager construction

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -20,24 +20,31 @@ class TestProtocolSetup(WithLogMonitoring, WithAsyncLoop):
         ctx = await Context.create_client_context()
         await ctx.shutdown()
 
-    @no_warnings
-    @asynctest
-    async def test_empty_contextmgr(self):
-        async with Context.create_client_context():
-            pass
+    # There is not yet a way to set things up in an async context manager.
+
+#     @no_warnings
+#     @asynctest
+#     async def test_empty_contextmgr(self):
+#         async with Context.create_client_context():
+#             pass
+
+    # The following tests should be converted to context managers once usable.
 
     @no_warnings
     @asynctest
     async def test_multiple_contexts(self):
         # Not that that'd be a regular thing to do, just checking it *can* be
         # done
-        async with Context.create_client_context():
-            async with Context.create_client_context():
-                # None is an acceptable site; binding to a concrete port
-                # removes the worries of situations where the default
-                # transports can't bind to "any".
-                async with Context.create_server_context(None, bind=("::1", None)):
-                    pass
+        c1 = await Context.create_client_context()
+        c2 = await Context.create_client_context()
+        # None is an acceptable site; binding to a concrete port
+        # removes the worries of situations where the default
+        # transports can't bind to "any".
+        s1 = await Context.create_server_context(None, bind=("::1", None))
+
+        await c1.shutdown()
+        await s1.shutdown()
+        await c2.shutdown()
 
     @no_warnings
     @asynctest
@@ -46,6 +53,8 @@ class TestProtocolSetup(WithLogMonitoring, WithAsyncLoop):
         #
         # (To some extent, that this is so easy is the fault of the weird way
         # the other protocols' ports are set for lack of configurability).
-        async with Context.create_server_context(None, bind=("::1", 1234)):
-            async with Context.create_server_context(None, bind=("::1", None)):
-                pass
+        s1 = await Context.create_server_context(None, bind=("::1", 1234))
+        s2 = await Context.create_server_context(None, bind=("::1", None))
+
+        await s1.shutdown()
+        await s2.shutdown()
