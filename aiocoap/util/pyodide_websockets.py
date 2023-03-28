753ae451cdc887087baac894d0376bd167c1b1b4
chrysn <chrysn@fsfe.org>
2023-03-28 17:30:20 +0200

    websockets: Error handling for pyodide

--- aiocoap/util/pyodide_websockets.py
+++ aiocoap/util/pyodide_websockets.py
@@ -29,18 +29,25 @@ class WebSocketClientProtocol(WebSocketCommonProtocol):
 
         self._socket = socket
         # FIXME: How do we get backpressure into the browser?
-        self._recv_queue = asyncio.Queue()
-        self._err_queue = asyncio.Queue()
-        self._close_future = asyncio.Future()
+        self._queue = asyncio.Queue()
 
         # The initial setting doesn't matter too much because we're not handing
         # it out before setting this to True ... still feels cleaner this way.
         self.open = False
 
     async def recv(self):
-        # FIXME: or error, and also handle text case?
-        event = await self._recv_queue.get()
-        return bytes((await event.data.arrayBuffer()).to_py())
+        (etype, event) = await self._queue.get()
+        if etype == "message":
+            if isinstance(event.data, str):
+                # FIXME: Test this
+                return event.data
+            return bytes((await event.data.arrayBuffer()).to_py())
+        elif etype == "close":
+            raise exceptions.ConnectionClosed()
+        elif etype == "error":
+            raise exceptions.WebSocketException("Connection error")
+        else:
+            raise RuntimeError("Unknown event in queue")
 
     async def send(self, msg):
         blob = js.Blob.new([js.Uint8Array.new(msg)])
@@ -48,6 +55,17 @@ class WebSocketClientProtocol(WebSocketCommonProtocol):
 
     # FIXME
     local_address = ("::", 1234)
+
+    def on_message(self, event):
+        self._queue.put_nowait(("message", event))
+
+    def on_error(self, event):
+        self.open = False
+        self._queue.put_nowait(("error", event))
+
+    def on_close(self, event):
+        self.open = False
+        self._queue.put_nowait(("close", event))
 
 async def connect(uri, subprotocols=None, ping_interval=20, ssl=None) -> WebSocketClientProtocol:
     if ssl is not None:
@@ -60,13 +78,13 @@ async def connect(uri, subprotocols=None, ping_interval=20, ssl=None) -> WebSock
 
     proto = WebSocketClientProtocol(socket)
 
-    opening = asyncio.Future()
-    add_event_listener(socket, "open", lambda e, opening=opening: opening.set_result(e))
-    add_event_listener(socket, "message", lambda e, queue=proto._recv_queue: queue.put_nowait(e))
-    add_event_listener(socket, "error", lambda e, queue=proto._err_queue: queue.put_nowait(e))
-    add_event_listener(socket, "close", lambda e, close=proto._close_future: close.set_result(e))
-    # FIXME: or error, or maybe even close?
-    await opening
+    add_event_listener(socket, "open", lambda e, q=proto._queue: q.put_nowait(("open", e)))
+    add_event_listener(socket, "message", proto.on_message)
+    add_event_listener(socket, "error", proto.on_error)
+    add_event_listener(socket, "close", proto.on_close)
+    (etype, event) = await proto._queue.get()
+    if etype != "open":
+        raise exceptions.WebSocketException("Failed to connect")
     proto.open = True
 
     return proto
