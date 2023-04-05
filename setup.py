b12e2f0fb998652265db7e6ee5e9f0439507b44b
chrysn <chrysn@fsfe.org>
2023-04-05 22:14:46 +0200

    setup: Move dependencies from setup.py to pyproject.toml; slim down doc
    This makes updating `all` a more manual process, but now that it's
    descriptive that's a bit more straightforward.
    
    `docs` was changed to not include `all` any more, because that's now
    easy to manage (given readthedocs is easier to configure).

--- setup.py
+++ setup.py
@@ -10,33 +10,6 @@
 
 from setuptools import setup, Command
 
-# When introducing something new, make sure to update doc/installation.rst
-extras_require = {
-        # Extra is still present for compatibility, but its dependency has been vendored in.
-        'linkheader': [],
-        # ge25519 is a workaround for
-        # <https://github.com/pyca/cryptography/issues/5557>; being pure python
-        # it's light enough to not warrant a dedicated group-oscore extra.
-        'oscore': ['cbor2', 'cryptography (>= 2.0)', 'filelock', 'ge25519'],
-        'tinydtls': ['DTLSSocket >= 0.1.11a1'],
-        'ws': ['websockets'],
-        'prettyprint': ['termcolor', 'cbor2', 'pygments', 'cbor-diag'],
-        'docs': ['sphinx', 'sphinx-argparse'], # extended below
-        'all': [], # populated below, contains everything but documentation dependencies for easier installation
-        }
-
-for k, v in extras_require.items():
-    if k.startswith(':') or k == 'all' or k == 'docs':
-        continue
-
-    # Most extras are required for docs to build. TinyDTLS is an exception
-    # because no module imports it at module level. (This is convenient also
-    # because TinyDTLS installation fails on readthedocs for unknown reasons.)
-    if k != 'tinydtls':
-        extras_require['docs'].extend(v)
-
-    extras_require['all'].extend(v)
-
 class Cite(Command):
     description = """Print how to cite aiocoap in a publication"""
 
@@ -65,8 +38,6 @@ class Cite(Command):
         }"""
 
 setup(
-    extras_require=extras_require,
-
     cmdclass={
         'cite': Cite,
         },
