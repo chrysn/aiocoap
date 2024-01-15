8bc3d9e1336015ae16701776f783a8d07a610d28
Codeglitches <80821713+Codeglitches@users.noreply.github.com>
2024-01-15 18:21:11 +0100

    Fix MyPy error as the urlparse library no longer exists in python3

--- aiocoap/util/vendored/link_header.py
+++ aiocoap/util/vendored/link_header.py
@@ -31,10 +31,7 @@ For further information see parse(), LinkHeader and Link.
 '''
 
 import re
-try:
-    from urllib.parse import urljoin
-except ImportError:
-    from urlparse import urljoin
+from urllib.parse import urljoin
 
 __all__ = ['parse', 'format_links', 'format_link', 'LinkHeader', 'Link', 'ParseException']
 
