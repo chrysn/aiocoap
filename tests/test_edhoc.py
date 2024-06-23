b9e78fb726749cfcc72135ae5253c9f1f74cf7e9
chrysn <chrysn@fsfe.org>
2024-06-23 20:23:01 +0200

    tests/edhoc: Test by-key-id and by-value CCSs

--- tests/test_edhoc.py
+++ tests/test_edhoc.py
@@ -15,6 +15,9 @@ from .test_server import TestServerBase, WithClient, WithTestServer
 from .fixtures import no_warnings
 
 class WithEdhocPair(WithTestServer, WithClient):
+    server_style: str
+    client_style: str
+
     def setUp(self):
         super().setUp()
 
@@ -39,7 +42,7 @@ class WithEdhocPair(WithTestServer, WithClient):
         client_entry = {"edhoc-oscore": {
                 "suite": 2,
                 "method": 3,
-                "own_cred_style": "by-key-id",
+                "own_cred_style": self.client_style,
                 "own_cred": clientccs,
                 "private_key_file": str(clientkey_file),
                 "peer_cred": serverccs,
@@ -51,7 +54,7 @@ class WithEdhocPair(WithTestServer, WithClient):
             'coap://%s/*' % self.servernetloc: {"edhoc-oscore": {
                 "suite": 2,
                 "method": 3,
-                "own_cred_style": "by-key-id",
+                "own_cred_style": self.server_style,
                 "own_cred": serverccs,
                 "private_key_file": str(serverkey_file),
                 }},
@@ -70,11 +73,27 @@ class WithEdhocPair(WithTestServer, WithClient):
 
 
 edhoc_modules = aiocoap.defaults.oscore_missing_modules()
+
 @unittest.skipIf(edhoc_modules, "EDHOC/OSCORE missing modules (%s)" % (edhoc_modules,))
-class TestServerEdhoc(TestServerBase, WithEdhocPair):
+class BaseServerEdhoc(TestServerBase, WithEdhocPair):
     @no_warnings
     def test_whoami_is_client(self):
         request = self.build_request()
         request.opt.uri_path = ['whoami']
         response = self.fetch_response(request)
         self.assertTrue(b":edhocclient" in response.payload, f"Expected to see own role in response, got {response.payload=}")
+
+class TestServerEdhocKidKid(BaseServerEdhoc):
+    server_style = 'by-key-id'
+    client_style = 'by-key-id'
+
+class TestServerEdhocValueValue(BaseServerEdhoc):
+    server_style = 'by-value'
+    client_style = 'by-value'
+
+class TestServerEdhocKidValue(BaseServerEdhoc):
+    server_style = 'by-key-id'
+    client_style = 'by-value'
+
+# that's not supposed to be tested, its child classes are
+del BaseServerEdhoc
