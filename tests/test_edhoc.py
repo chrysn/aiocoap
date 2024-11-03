2c85993a080f878469ca3a0d027c8fbe9b05ae9b
chrysn <chrysn@fsfe.org>
2024-11-03 01:47:36 +0000

    tests/edhoc: Add test for unauthenticated peers

--- tests/test_edhoc.py
+++ tests/test_edhoc.py
@@ -18,6 +18,9 @@ class WithEdhocPair(WithTestServer, WithClient):
     server_style: str
     client_style: str
 
+    client_knows_server = True
+    server_knows_client = True
+
     def setUp(self):
         super().setUp()
 
@@ -33,10 +36,18 @@ class WithEdhocPair(WithTestServer, WithClient):
         serverkey_file = tmpbase / "server.cosekey"
         serverkey = edhoc.CoseKeyForEdhoc.generate(serverkey_file)
         serverccs = serverkey.as_ccs(kid=bytes.fromhex("00"), subject="s")
+        if self.client_knows_server:
+            serverccs_for_client = serverccs
+        else:
+            serverccs_for_client = {"unauthenticated": True}
 
         clientkey_file = tmpbase / "client.cosekey"
         clientkey = edhoc.CoseKeyForEdhoc.generate(clientkey_file)
         clientccs = clientkey.as_ccs(kid=bytes.fromhex("01"), subject="c")
+        if self.server_knows_client:
+            clientccs_for_server = clientccs
+        else:
+            clientccs_for_server = {"unauthenticated": True}
 
         # FIXME shouldn't that only need the servernamealias?
         client_entry = {
@@ -46,7 +57,7 @@ class WithEdhocPair(WithTestServer, WithClient):
                 "own_cred_style": self.client_style,
                 "own_cred": clientccs,
                 "private_key_file": str(clientkey_file),
-                "peer_cred": serverccs,
+                "peer_cred": serverccs_for_client,
             }
         }
         self.client.client_credentials.load_from_dict(
@@ -71,7 +82,7 @@ class WithEdhocPair(WithTestServer, WithClient):
                     "edhoc-oscore": {
                         "suite": 2,
                         "method": 3,
-                        "peer_cred": clientccs,
+                        "peer_cred": clientccs_for_server,
                     }
                 },
             }
@@ -112,9 +123,16 @@ class TestServerEdhocValueValue(BaseServerEdhoc):
     client_style = "by-value"
 
 
-class TestServerEdhocKidValue(BaseServerEdhoc):
+class TestServerEdhocAnonKid(BaseServerEdhoc):
+    server_style = "by-value"
+    client_style = "by-key-id"
+    client_knows_server = False
+
+
+class TestServerEdhocKidAnon(BaseServerEdhoc):
     server_style = "by-key-id"
     client_style = "by-value"
+    server_knows_client = False
 
 
 # that's not supposed to be tested, its child classes are
