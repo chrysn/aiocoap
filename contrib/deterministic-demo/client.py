debaaed4794edd8bcdc0bfa981231818b0775c18
chrysn <chrysn@fsfe.org>
2025-06-29 16:07:18 +0200

    cacheable: Update client to use credential from test vectors

--- contrib/deterministic-demo/client.py
+++ contrib/deterministic-demo/client.py
@@ -18,80 +18,60 @@ async def main():
 
     import aiocoap.oscore
 
-    # Acting as Rikard Test 2 Entity 3
-    protocol.client_credentials["coap://localhost/*"] = (
-        aiocoap.oscore.SimpleGroupContext(
-            alg_aead=aiocoap.oscore.algorithms[aiocoap.oscore.DEFAULT_ALGORITHM],
-            hashfun=aiocoap.oscore.hashfunctions[aiocoap.oscore.DEFAULT_HASHFUNCTION],
-            alg_signature=aiocoap.oscore.Ed25519(),
-            alg_group_enc=aiocoap.oscore.algorithms[aiocoap.oscore.DEFAULT_ALGORITHM],
-            alg_pairwise_key_agreement=aiocoap.oscore.EcdhSsHkdf256(),
-            group_id=bytes.fromhex("DD11"),
-            master_secret=bytes.fromhex("11223344556677889900AABBCCDDEEFF"),
-            master_salt=bytes.fromhex("1F2E3D4C5B6A7081"),
-            # these are really moot given we only access this as a deterministic client
-            sender_id=bytes.fromhex("52"),
-            private_key=bytes.fromhex(
-                "E550CD532B881D52AD75CE7B91171063E568F2531FBDFB32EE01D1910BCF810F"
-            ),
-            peers={
-                bytes.fromhex("0A"): cbor2.dumps(
-                    {
-                        8: {
-                            1: {
-                                1: 1,
-                                3: -8,
-                                -1: 6,
-                                -2: bytes.fromhex(
-                                    "CE616F28426EF24EDB51DBCEF7A23305F886F657959D4DF889DDFC0255042159"
-                                ),
-                            }
-                        }
-                    }
-                ),
-                bytes.fromhex("51"): cbor2.dumps(
-                    {
-                        8: {
-                            1: {
-                                1: 1,
-                                3: -8,
-                                -1: 6,
-                                -2: bytes.fromhex(
-                                    "2668BA6CA302F14E952228DA1250A890C143FDBA4DAED27246188B9E42C94B6D"
-                                ),
-                            }
-                        }
-                    }
-                ),
-                # that's a new one for deterministic, and we build a key for it but no shared secret
-                bytes.fromhex("dc"): aiocoap.oscore.DETERMINISTIC_KEY,
-            },
-            # requests in group mode would be sent with
-            #                    ).pairwise_for(bytes.fromhex('0A'))
-            # but here we want to request deterministically
-            sender_auth_cred=cbor2.dumps(
-                {
-                    8: {
-                        1: {
-                            1: 1,
-                            3: -8,
-                            -1: 6,
-                            -2: bytes.fromhex(
-                                "5394E43633CDAC96F05120EA9F21307C9355A1B66B60A834B53E9BF60B1FB7DF"
-                            ),
-                        }
+    protocol.client_credentials["coap://*"] = aiocoap.oscore.SimpleGroupContext(
+        alg_aead=aiocoap.oscore.algorithms[aiocoap.oscore.DEFAULT_ALGORITHM],
+        hashfun=aiocoap.oscore.hashfunctions[aiocoap.oscore.DEFAULT_HASHFUNCTION],
+        alg_signature=aiocoap.oscore.Ed25519(),
+        alg_group_enc=aiocoap.oscore.algorithms[aiocoap.oscore.DEFAULT_ALGORITHM],
+        alg_pairwise_key_agreement=aiocoap.oscore.EcdhSsHkdf256(),
+        group_id=bytes.fromhex("dd11"),
+        master_secret=bytes.fromhex("0102030405060708090a0b0c0d0e0f10"),
+        master_salt=bytes.fromhex("9e7ca92223786340"),
+        # These are really moot given we only access this as a deterministic
+        # client, but SimpleGroupContext doesn't yet understand that it could
+        # be a Silent Server (which is the closest thing to a
+        # deterministic-only client in the group spec)…
+        sender_id=bytes.fromhex("f000"),
+        private_key=bytes.fromhex(
+            "E550CD532B881D52AD75CE7B91171063E568F2531FBDFB32EE01D1910BCF810F"
+        ),
+        sender_auth_cred=cbor2.dumps(
+            {
+                8: {
+                    1: {
+                        1: 1,
+                        3: -8,
+                        -1: 6,
+                        -2: bytes.fromhex(
+                            "5394E43633CDAC96F05120EA9F21307C9355A1B66B60A834B53E9BF60B1FB7DF"
+                        ),
                     }
                 }
-            ),
-            group_manager_cred=b"This can be an arbitrary byte string as long as nobody interacts with the GM and all agree what it is.",
-        ).for_sending_deterministic_requests(bytes.fromhex("dc"), bytes.fromhex("0a"))
-    )
+            },
+            canonical=True,
+        ),
+        # … but we do need those:
+        peers={
+            bytes.fromhex("dc"): aiocoap.oscore.DETERMINISTIC_KEY,
+            bytes.fromhex("52"): bytes.fromhex("""
+                    a501781a636f6170733a2f2f7365727665722e6578616d706c652e636f6d
+                    026673656e64657203781a636f6170733a2f2f636c69656e742e6578616d
+                    706c652e6f7267041a70004b4f08a101a401010327200621582077ec358c
+                    1d344e41ee0e87b8383d23a2099acd39bdf989ce45b52e887463389b"""),
+        },
+        group_manager_cred=bytes.fromhex("""
+                a501781a636f6170733a2f2f6d79736974652e6578616d706c652e636f6d
+                026c67726f75706d616e6167657203781a636f6170733a2f2f646f6d6169
+                6e2e6578616d706c652e6f7267041aab9b154f08a101a401010327200621
+                5820cde3efd3bc3f99c9c9ee210415c6cba55061b5046e963b8a58c9143a
+                61166472"""),
+    ).for_sending_deterministic_requests(bytes.fromhex("dc"), bytes.fromhex("52"))
 
-    request = Message(code=GET, uri="coap://localhost/.well-known/core")
+    request = Message(code=GET, uri="coap://localhost/helloWorld")
     response = await protocol.request(request).response
     print("Got response", response, response.payload)
 
-    request = Message(code=GET, uri="coap://localhost/.well-known/core")
+    request = Message(code=GET, uri="coap://localhost/helloWorld")
     response = await protocol.request(request).response
     print("Got response", response, response.payload)
 
--- contrib/deterministic-demo/server.py
+++ contrib/deterministic-demo/server.py
@@ -16,13 +16,18 @@ logging.basicConfig(level=logging.INFO)
 logging.getLogger("coap-server").setLevel(logging.DEBUG)
 
 
-async def main():
-    # Resource tree creation
-    root = resource.Site()
+# The resource from the upcoming draft-amsuess-core-cachable-oscore-11
+class HelloWorld(resource.Resource):
+    async def render_get(self, request):
+        return aiocoap.Message(payload=b". ID: 42", content_format=0)
 
+
+async def main():
+    root = resource.Site()
     root.add_resource(
         [".well-known", "core"], resource.WKCResource(root.get_resources_as_linkheader)
     )
+    root.add_resource(["helloWorld"], HelloWorld())
 
     server_credentials = aiocoap.credentials.CredentialsMap()
 
@@ -30,66 +35,33 @@ async def main():
 
     protocol = await aiocoap.Context.create_server_context(root)
 
-    # Keys from IETF109 plug test: Rikard Test 2 Entity 1
     server_credentials[":a"] = aiocoap.oscore.SimpleGroupContext(
         alg_aead=aiocoap.oscore.algorithms[aiocoap.oscore.DEFAULT_ALGORITHM],
         hashfun=aiocoap.oscore.hashfunctions[aiocoap.oscore.DEFAULT_HASHFUNCTION],
         alg_signature=aiocoap.oscore.Ed25519(),
         alg_group_enc=aiocoap.oscore.algorithms[aiocoap.oscore.DEFAULT_ALGORITHM],
         alg_pairwise_key_agreement=aiocoap.oscore.EcdhSsHkdf256(),
-        group_id=bytes.fromhex("DD11"),
-        master_secret=bytes.fromhex("11223344556677889900AABBCCDDEEFF"),
-        master_salt=bytes.fromhex("1F2E3D4C5B6A7081"),
-        sender_id=bytes.fromhex("0A"),
+        group_id=bytes.fromhex("dd11"),
+        master_secret=bytes.fromhex("0102030405060708090a0b0c0d0e0f10"),
+        master_salt=bytes.fromhex("9e7ca92223786340"),
+        sender_id=bytes.fromhex("52"),
         private_key=bytes.fromhex(
-            "397CEB5A8D21D74A9258C20C33FC45AB152B02CF479B2E3081285F77454CF347"
+            "857eb61d3f6d70a278a36740d132c099f62880ed497e27bdfd4685fa1a304f26"
         ),
         peers={
-            bytes.fromhex("51"): cbor2.dumps(
-                {
-                    8: {
-                        1: {
-                            1: 1,
-                            3: -8,
-                            -1: 6,
-                            -2: bytes.fromhex(
-                                "2668BA6CA302F14E952228DA1250A890C143FDBA4DAED27246188B9E42C94B6D"
-                            ),
-                        }
-                    }
-                }
-            ),
-            bytes.fromhex("52"): cbor2.dumps(
-                {
-                    8: {
-                        1: {
-                            1: 1,
-                            3: -8,
-                            -1: 6,
-                            -2: bytes.fromhex(
-                                "5394E43633CDAC96F05120EA9F21307C9355A1B66B60A834B53E9BF60B1FB7DF"
-                            ),
-                        }
-                    }
-                }
-            ),
             bytes.fromhex("dc"): aiocoap.oscore.DETERMINISTIC_KEY,
         },
-        sender_auth_cred=cbor2.dumps(
-            {
-                8: {
-                    1: {
-                        1: 1,
-                        3: -8,
-                        -1: 6,
-                        -2: bytes.fromhex(
-                            "CE616F28426EF24EDB51DBCEF7A23305F886F657959D4DF889DDFC0255042159"
-                        ),
-                    }
-                }
-            }
-        ),
-        group_manager_cred=b"This can be an arbitrary byte string as long as nobody interacts with the GM and all agree what it is.",
+        sender_auth_cred=bytes.fromhex("""
+                    a501781a636f6170733a2f2f7365727665722e6578616d706c652e636f6d
+                    026673656e64657203781a636f6170733a2f2f636c69656e742e6578616d
+                    706c652e6f7267041a70004b4f08a101a401010327200621582077ec358c
+                    1d344e41ee0e87b8383d23a2099acd39bdf989ce45b52e887463389b"""),
+        group_manager_cred=bytes.fromhex("""
+                a501781a636f6170733a2f2f6d79736974652e6578616d706c652e636f6d
+                026c67726f75706d616e6167657203781a636f6170733a2f2f646f6d6169
+                6e2e6578616d706c652e6f7267041aab9b154f08a101a401010327200621
+                5820cde3efd3bc3f99c9c9ee210415c6cba55061b5046e963b8a58c9143a
+                61166472"""),
     )
 
     await asyncio.get_running_loop().create_future()
