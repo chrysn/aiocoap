2fed5108a067b30122a552398ec6209d4671d8d7
chrysn <chrysn@fsfe.org>
2025-09-14 10:19:59 +0200

    util: Limit workaround to versions < 3.14
    With all required constants re-exported since
    <https://github.com/python/cpython/pull/118987>, this puts an upper
    bound on workarounded version to be sure we use it when available.
    
    The `IN` module access is removed because it was just a private module
    anyway.

--- src/aiocoap/util/socknumbers.py
+++ src/aiocoap/util/socknumbers.py
@@ -27,16 +27,14 @@ dropped (see [issue 352](https://github.com/chrysn/aiocoap/issues/352)).
 import sys
 
 try:
-    from IN import IPV6_RECVERR, IP_RECVERR  # type: ignore
+    from socket import IPV6_RECVERR, IP_RECVERR, MSG_ERRQUEUE  # type: ignore
 except ImportError:
-    if sys.platform == "linux":
-        IPV6_RECVERR = 25  # type: ignore
-        IP_RECVERR = 11  # type: ignore
-
-try:
-    from socket import MSG_ERRQUEUE  # noqa: F401
-except ImportError:
-    pass
+    if sys.version_info < (3, 14):
+        if sys.platform == "linux":
+            IPV6_RECVERR = 25  # type: ignore
+            IP_RECVERR = 11  # type: ignore
+            MSG_ERRQUEUE = 8192  # type: ignore
+        # This is not available on other platforms.
 
 HAS_RECVERR = "IP_RECVERR" in locals() and "MSG_ERRQUEUE" in locals()
 """Indicates whether the discovered constants indicate that the Linux
