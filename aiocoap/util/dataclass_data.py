5e4689e0299c3b1a3c534f5e90d36599fb99cc0b
chrysn <chrysn@fsfe.org>
2026-01-09 10:21:21 +0100

    Add dataclass_data utility for upcoming transport configuration

---
+++ aiocoap/util/dataclass_data.py
@@ -0,0 +1,108 @@
+# SPDX-FileCopyrightText: Christian Amsüss and the aiocoap contributors
+#
+# SPDX-License-Identifier: MIT
+
+"""Tools to load a typed dataclass from CBOR/JSON/TOML/YAML-model data.
+
+Unlike what is in the aiocoap.credentials module, this works from the fixed
+assumption that the item is a dataclass (as opposed to having an arbitrary
+constructor), which should ease things.
+
+**Caveats**: The module expects the data classes' annotations to be types and
+not strings, and therefore can't be used with types defined under `from
+__future__ import annotations`.
+
+>>> from dataclasses import dataclass
+>>> @dataclass
+... class Inner(LoadStoreClass):
+...     some_text: str
+...     some_number: int
+>>> @dataclass
+... class Top(LoadStoreClass):
+...     x: str
+...     y: Inner
+>>> Top.load({"x": "test", "y": {"some-text": "one", "some-number": 42}})
+Top(x='test', y=Inner(some_text='one', some_number=42))
+"""
+
+import dataclasses
+
+
+class LoadStoreClass:
+    @classmethod
+    def load(
+        cls, data: dict, prefix: str = "", depth_limit: int = 16
+    ) -> "LoadStoreClass":
+        """Creates an instance from the given data dictionary.
+
+        Keys are used to populate fields like in the initializer; dashes ("-")
+        in names are replaced with underscores ("_") so that Python-idiomatic
+        field names (in snake_case) can be used with TOML idiomatic item names
+        (in kebab-case).
+
+        Values are type-checked against the annotations, and unknown fields are
+        disallowed. When annotations indicate another ``LoadStoreClass``,
+        initialization recurses into that type up to a depth limit.
+
+        The ``prefix`` is used for error messages: It builds up in the recursive
+        build process and thus gives the user concrete guidance as to where in
+        the top-level item the trouble was. For data loaded from files, it is
+        prudent to give the file name in this argument.
+
+        This reliably raises ``ValueError`` or its subtypes on unacceptable data.
+        """
+
+        assert dataclasses.is_dataclass(cls)
+
+        prefix = f"{prefix}/" if prefix else prefix
+
+        fields = {f.name: f for f in dataclasses.fields(cls)}
+
+        kwargs = {}
+
+        for key, value in data.items():
+            keyprefix = f"{prefix}{key}"
+            f = key.replace("-", "_")
+            fieldtype = fields[f].type
+            if issubclass(fieldtype, LoadStoreClass):
+                # FIXME: allow annotating single distinct non-dict-value, eg.
+                # like Cargo.toml's implicit version in dependencies
+                if not isinstance(value, dict):
+                    raise ValueError(
+                        f"Type mismatch on {keyprefix}: Expected dictionary to populate {fieldtype.__name__} with, found {type(value).__name__}"
+                    )
+                if depth_limit == 0:
+                    raise ValueError("Nesting exceeded limit in {keyprefix}")
+                value = fieldtype.load(
+                    value, prefix=keyprefix, depth_limit=depth_limit - 1
+                )
+            else:
+                # FIXME:
+                # - For lists and dicts, evaluate items (can be deferred until we actually have any)
+                # - Special treatment for bytes: Accept {"acii": "hello"} and {"hex": "001122"}
+                # - Special treatment for Path (probably with new filename argument)
+                # - Handle Optional
+                # - Handle unions, possibly fanning out by disambiguator keys?
+                if not isinstance(value, fieldtype):
+                    raise ValueError(
+                        f"Type mismatch on {keyprefix}: Expected {fieldtype.__name__}, found {type(value).__name__}"
+                    )
+            kwargs[f] = value
+
+        try:
+            return cls(**kwargs)
+        except TypeError as e:
+            missing = [
+                f.name
+                for f in dataclasses.fields(cls)
+                if f.name not in kwargs and f.default is dataclasses.MISSING
+            ]
+            if missing:
+                # The likely case -- what else might go wrong?
+                raise ValueError(
+                    f"Construcintg an instance of {cls.__name__} at {keyprefix}, these items are missing: {', '.join(m.replace('_', '-') for m in missing)}"
+                )
+            else:
+                raise ValueError(
+                    f"Constructing instance of {cls.__name__} at {keyprefix} failed for unexpected reasons"
+                ) from e
---
+++ tests/test_dataclassdata.py
@@ -0,0 +1,87 @@
+# SPDX-FileCopyrightText: Christian Amsüss and the aiocoap contributors
+#
+# SPDX-License-Identifier: MIT
+
+import unittest
+from dataclasses import dataclass
+
+from aiocoap.util.dataclass_data import LoadStoreClass
+
+
+@dataclass
+class Inner2(LoadStoreClass):
+    a: int
+    b: str
+
+
+@dataclass
+class Inner1(LoadStoreClass):
+    x: Inner2
+    y: Inner2
+    z: float
+
+
+@dataclass
+class Outer(LoadStoreClass):
+    label: str
+    inner: Inner1
+
+
+class TestDataclassData(unittest.TestCase):
+    def test_valid(self):
+        self.assertEqual(
+            Outer.load(
+                {
+                    "label": "hello",
+                    "inner": {
+                        "x": {"a": 1, "b": "x"},
+                        "y": {"a": 2, "b": "X"},
+                        "z": 1.5,
+                    },
+                }
+            ),
+            Outer(
+                label="hello",
+                inner=Inner1(x=Inner2(a=1, b="x"), y=Inner2(a=2, b="X"), z=1.5),
+            ),
+        )
+
+    def test_type(self):
+        try:
+            Outer.load(
+                {
+                    "label": "hello",
+                    "inner": {
+                        "x": {"a": 1, "b": "x"},
+                        "y": {"a": 2, "b": 42},
+                        "z": 1.5,
+                    },
+                }
+            )
+        except ValueError as e:
+            if "inner/y/b" in str(e) and "Expected str, found int" in str(e):
+                pass
+            else:
+                raise Exception("Path to missing field was not shown") from e
+        else:
+            raise Exception("Incomplete data was loaded")
+
+    def test_missing(self):
+        try:
+            Outer.load(
+                {
+                    "label": "hello",
+                    "inner": {
+                        "x": {"a": 1, "b": "x"},
+                        "y": {"a": 2},  # no "b"
+                        "z": 1.5,
+                    },
+                }
+            )
+        except ValueError as e:
+            if "inner/y" in str(e) and "missing: b":
+                pass
+            else:
+                raise Exception("Path to missing field was not shown") from e
+        else:
+            raise Exception("Incomplete data was loaded")
